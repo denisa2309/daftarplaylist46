92cf18164975ceb135c4cbe12637ad56fcfa3bbf
Amelia Rizky Yuniar <lliameliia.2212@gmail.com>
2025-06-13 03:39:16 +0800

    style: optimize search input layout in Navbar component

--- src/elements/Navbar.jsx
+++ src/elements/Navbar.jsx
@@ -22,13 +22,11 @@ const Navbar = () => {
 
       {/* Search input with icon */}
       <div className='relative w-1/3'>
-        <div className='absolute inset-y-0 left-0 flex items-center pl-3'>
-          <GoSearch className='text-gray-500 text-xl' />
-        </div>
+        <GoSearch className='absolute left-0 -top-0 transform -translate-y-1/3 text-gray-500 text-xl' />
         <input
           type='text'
           placeholder='Cari playlist...'
-          className='w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-500'
+          className='w-full pl-10 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-500'
         />
       </div>
 
