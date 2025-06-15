da4098666575d9f553413c8bc7c8e65d2d31e1fc
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-06-16 02:29:45 +0800

    add index.js to export core components for easier imports

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+export { default as ErrorHandler } from './ErrorHandler';
+export { default as Loading } from './Loading';
+export { default as Pagination } from './Pagination';
+export { default as SearchBar } from './SearchBar';
+export { default as SearchForeign } from './SearchForeign';
---
+++ src/elements/index.js
@@ -0,0 +1,4 @@
+export { default as Card } from './Card';
+export { default as Footer } from './Footer';
+export { default as Form } from './Form';
+export { default as Navbar } from './Navbar';
